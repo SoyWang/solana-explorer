b08fb5ed8691fc1fdf9c9b0731f1043fe6556a90
Soy <wsnuli@126.com>
2022-08-03 18:18:12 +0800

    init

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,17 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+A
+public/index.html,f\4\f43ecdf8d666ee908aacce037b4cd72159e4741b
+=
+src/index.tsx,5\a\5ab44fe33ca96299f51ad4778a73e425b92bf439
+<
+package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
+L
+src/components/SearchBar.tsx,2\b\2b3428e335438675a9388a3b458140705318dad7
+I
+src/components/Navbar.tsx,d\b\dbf6f648b7a7ab0750c83f8a1ea7790c0c7e10cf
+O
+src/components/ClusterModal.tsx,1\d\1d06a6164b71ec0fcb32f07891db77ee147a456c
+I
+src/providers/cluster.tsx,3\7\37151cadb4a59550689decf3e008e081bfc714a7
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+// import Logo from "img/logos-solana/dark-explorer-logo.svg";
+import Logo from "img/scan/logo-solana-scan.ae03f445.svg";
+import { clusterPath } from "utils/url";
+import { Link, NavLink } from "react-router-dom";
+import { ClusterStatusButton } from "components/ClusterStatusButton";
+
+export function Navbar() {
+  // TODO: use `collapsing` to animate collapsible navbar
+  const [collapse, setCollapse] = React.useState(false);
+
+  return (
+    <nav className="navbar navbar-expand-md navbar-light align-items-start">
+      <div className="container">
+        <Link to={clusterPath("/")}>
+          <img src={Logo} width="130" alt="Solana Explorer" />
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={() => setCollapse((value) => !value)}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className={`collapse navbar-collapse ml-auto mr-4 ${
+            collapse ? "show" : ""
+          }`}
+        >
+          {/* <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" to={clusterPath("/")} exact>
+                Cluster Stats
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to={clusterPath("/supply")}>
+                Supply
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to={clusterPath("/tx/inspector")}>
+                Inspector
+              </NavLink>
+            </li>
+          </ul> */}
+        </div>
+
+        <div className="d-none d-md-block">
+          <ClusterStatusButton />
+        </div>
+      </div>
+    </nav>
+  );
+}
